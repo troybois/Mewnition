b97ebf4d7805386189e9be78a3f7f4c1eceaf563
Jared Flores <jared_flores@cs.ucsb.edu>
2020-02-29 12:15:06 -0800

    explosions!

--- docs/pc/js/game.js
+++ docs/pc/js/game.js
@@ -52,9 +52,17 @@ function win_load() {
 		var ARM_OFFSET_X = 16;
 		var ARM_OFFSET_Y = 16;
 
+		var GRENADE_HEIGHT = 30;
+		var GRENADE_HHEIGHT = GRENADE_HEIGHT / 2;
+		var GRENADE_WIDTH = 30;
+		var GRENADE_HWIDTH = GRENADE_WIDTH / 2;
+
 		var GRAVITY = .006;
 		var JUMP_DY = -1.8;
 		var PLAYER_DX = .5;
+		var SHOT_GRAVITY = .003;
+		var SHOT_VELOCITY = 1.4;
+		var SHOT_COOLDOWN = 1000 * 5;
 
 		var IDLE_FPS = 120;
 		var RUN_FPS = 60;
@@ -70,6 +78,14 @@ function win_load() {
 		var MID_MB_DOWN = false;
 		var RIGHT_MB_DOWN = false;
 
+		var ANGLES = [];
+		for( i = 0; i < 360; i++ ) {
+			ANGLES.push( [] );
+			ANGLES[ i ].push( i / 180 * Math.PI );
+			ANGLES[ i ].push( Math.cos( i / 180 * Math.PI ) );
+			ANGLES[ i ].push( Math.sin( i / 180 * Math.PI ) );
+		}
+
 		function Player( pid ) {
 			this.x = pid * BG_WIDTH / 2;
 			this.y = CANVAS_HEIGHT - PLAYER_HEIGHT;
@@ -83,11 +99,14 @@ function win_load() {
 			this.right = true;
 			this.hitboxes = [];
 			this.jumping = false;
+			this.last_shot = -1;
 		}
 
 		function Grenade() {
 			this.x = 0;
 			this.y = 0;
+			this.dx = 0;
+			this.dy = 0;
 			this.active = false;
 			this.hitboxes = [];
 		}
@@ -151,6 +170,18 @@ function win_load() {
 		var cursor_x = 0;
 		var cursor_y = 0;
 
+		var HB_PLAYER_TOP = 21;
+		var HB_PLAYER_BOT = 96;
+		var HB_PLAYER_RLEFT = 36;
+		var HB_PLAYER_RRIGHT = 63;
+		var HB_PLAYER_LLEFT = 33;
+		var HB_PLAYER_LRIGHT = 60;
+
+		var HB_GRENADE_TOP = 0;
+		var HB_GRENADE_BOT = 30;
+		var HB_GRENADE_LEFT = 0;
+		var HB_GRENADE_RIGHT = 30;
+
 		var last_ts = -1, ticks, curr_ts;
 		function update_player( id ) {
 			var p, new_x, new_y, new_dx = 0, new_dy = 0;
@@ -182,15 +213,22 @@ function win_load() {
 					new_dx = 0;
 					p.anim = "idle";
 				}
+				var right = HB_PLAYER_LRIGHT;
+				var left = HB_PLAYER_LLEFT;
+				if( p.right ) {
+					right = HB_PLAYER_RRIGHT;
+					left = HB_PLAYER_RLEFT;
+				}
 				var done = false;
 				new_x += new_dx * ticks;
 				if( new_x > p.x ) {
-					var boundary = ( ( new_x + PLAYER_WIDTH ) / TILE_SIZE ) | 0;
-					var axis = ( p.y / TILE_SIZE ) | 0;
-					for( i = ( ( p.x + PLAYER_WIDTH ) / TILE_SIZE ) | 0; i <= boundary; i++ ) {
-						for( j = axis; j < axis + 4; j++ ) {
+					var boundary = ( ( new_x + right ) / TILE_SIZE ) | 0;
+					var axis = ( ( p.y + HB_PLAYER_TOP ) / TILE_SIZE ) | 0;
+					var axis2 = ( ( p.y + PLAYER_HEIGHT ) / TILE_SIZE ) | 0;
+					for( i = ( ( p.x + right ) / TILE_SIZE ) | 0; i <= boundary; i++ ) {
+						for( j = axis; j < axis2; j++ ) {
 							if( tiles[ j ][ i % TILE_WIDTH ] ) {
-								new_x = i * TILE_SIZE - PLAYER_WIDTH;
+								new_x = i * TILE_SIZE - right;
 								new_dx = 0;
 								done = true;
 								break;
@@ -199,12 +237,13 @@ function win_load() {
 						if( done ) break;
 					}
 				} else if( new_x < p.x ) {
-					var boundary = ( new_x / TILE_SIZE ) | 0;
-					var axis = ( p.y / TILE_SIZE ) | 0;
-					for( i = ( p.x / TILE_SIZE ) | 0; i >= boundary; i-- ) {
-						for( j = axis; j < axis + 4; j++ ) {
+					var boundary = ( ( new_x + left ) / TILE_SIZE ) | 0;
+					var axis = ( ( p.y + HB_PLAYER_TOP ) / TILE_SIZE ) | 0;
+					var axis2 = ( ( p.y + PLAYER_HEIGHT ) / TILE_SIZE ) | 0;
+					for( i = ( ( p.x + left ) / TILE_SIZE ) | 0; i >= boundary; i-- ) {
+						for( j = axis; j < axis2; j++ ) {
 							if( tiles[ j ][ i ] ) {
-								new_x = ( i + 1 ) * TILE_SIZE;
+								new_x = ( i + 1 ) * TILE_SIZE - left;
 								new_dx = 0;
 								done = true;
 								break;
@@ -227,9 +266,10 @@ function win_load() {
 				} else {
 					if( new_y > p.y ) {
 						var boundary = ( ( new_y + PLAYER_HEIGHT ) / TILE_SIZE ) | 0;
-						var axis = ( p.x / TILE_SIZE ) | 0;
+						var axis = ( ( p.x + left ) / TILE_SIZE ) | 0;
+						var axis2 = ( ( p.x + right - 1 ) / TILE_SIZE ) | 0;
 						for( j = ( ( p.y + PLAYER_HEIGHT ) / TILE_SIZE ) | 0; j <= boundary; j++ ) {
-							for( i = axis; i < axis + 4; i++ ) {
+							for( i = axis; i <= axis2; i++ ) {
 								if( tiles[ j ][ i % TILE_WIDTH ] ) {
 									new_y = j * TILE_SIZE - PLAYER_HEIGHT;
 									new_dy = 0;
@@ -241,12 +281,13 @@ function win_load() {
 							if( done ) break;
 						}
 					} else if( new_y < p.y ) {
-						var boundary = ( new_y / TILE_SIZE ) | 0;
-						var axis = ( p.x / TILE_SIZE ) | 0;
+						var boundary = ( ( new_y + HB_PLAYER_TOP ) / TILE_SIZE ) | 0;
+						var axis = ( ( p.x + left ) / TILE_SIZE ) | 0;
+						var axis2 = ( ( p.x + right - 1 ) / TILE_SIZE ) | 0;
 						for( j = ( p.y / TILE_SIZE ) | 0; j >= boundary; j-- ) {
-							for( i = axis; i < axis + 4; i++ ) {
+							for( i = axis; i <= axis2; i++ ) {
 								if( tiles[ j ][ i ] ) {
-									new_y = ( j + 1 ) * TILE_SIZE;
+									new_y = ( j + 1 ) * TILE_SIZE - HB_PLAYER_TOP;
 									new_dy = 0;
 									done = true;
 									break;
@@ -260,6 +301,87 @@ function win_load() {
 				p.dy = new_dy;
 				p.x = new_x;
 				p.y = new_y;
+			}
+		}
+
+		function update_grenade( id ) {
+			var p, g, new_x = 0, new_y = 0, new_dx = 0, new_dy = 0;
+			if( PLAYER_COUNT > id ) {
+				g = grenades[ id ];
+				p = players[ id ];
+				if( g.active ) {
+					new_x = g.x;
+					new_y = g.y;
+					new_dx = g.dx;
+					new_dy = g.dy;
+					new_dy += SHOT_GRAVITY * ticks;
+					new_x += new_dx * ticks;
+					new_y += new_dy * ticks;
+				} else if( id == ME ) {
+					if( LEFT_MB_DOWN ) {
+						var passed;
+						if( p.last_shot == -1 ) p.last_shot = curr_ts - SHOT_COOLDOWN;
+						passed = curr_ts - p.last_shot;
+						if( passed >= SHOT_COOLDOWN ) {
+							new_dx = ANGLES[ p.arm ][ 1 ];
+							new_dy = ANGLES[ p.arm ][ 2 ];
+							new_x = p.x + 32 + new_dx * 20;
+							new_y = p.y + 38 + new_dy * 20;
+							new_dx *= SHOT_VELOCITY;
+							new_dy *= SHOT_VELOCITY;
+							g.active = true;
+							p.last_shot = curr_ts;
+						}
+					}
+				}
+				new_x %= CANVAS_WIDTH;
+				if( new_x < 0 ) new_x += CANVAS_WIDTH;
+				if( new_y < 0 ) new_y += CANVAS_HEIGHT;
+				var tile_ul_x = ( new_x / TILE_SIZE ) | 0, tile_ul_y = ( new_y / TILE_SIZE ) | 0;
+				var tile_ur_x = ( ( new_x + GRENADE_WIDTH ) / TILE_SIZE ) | 0, tile_ur_y = ( new_y / TILE_SIZE ) | 0;
+				var tile_br_x = ( ( new_x + GRENADE_WIDTH ) / TILE_SIZE ) | 0, tile_br_y = ( ( new_y + GRENADE_HEIGHT ) / TILE_SIZE ) | 0;
+				var tile_bl_x = ( new_x / TILE_SIZE ) | 0, tile_bl_y = ( ( new_y + GRENADE_HEIGHT ) / TILE_SIZE ) | 0;
+				var explode = false, explode_x = ( ( new_x + GRENADE_HWIDTH ) / TILE_SIZE ) | 0, explode_y = ( ( new_y + GRENADE_HWIDTH ) / TILE_SIZE ) | 0;
+				if( new_y + GRENADE_HEIGHT >= CANVAS_HEIGHT ) {
+					g.active = false;
+					explode = true;
+					explode_y = TILE_HEIGHT - 1;
+				} else if( tiles[ tile_ul_y ][ tile_ul_x ] ) {
+					g.active = false;
+					explode = true;
+				} else if( tiles[ tile_ur_y ][ tile_ur_x ] ) {
+					g.active = false;
+					explode = true;
+				} else if( tiles[ tile_br_y ][ tile_br_x ] ) {
+					g.active = false;
+					explode = true;
+				} else if( tiles[ tile_bl_y ][ tile_bl_x ] ) {
+					g.active = false;
+					explode = true;
+				} else if( new_y <= 0 ) {
+					g.active = false;
+					explode = true;
+				}
+				var rad;
+				if( explode ) {
+					for( j = explode_y - 3; j <= explode_y + 3; j++ ) {
+						rad = Math.max( Math.abs( explode_y - j ) - 1, 0 );
+						for( i = explode_x - ( 3 - rad ); i <= explode_x + ( 3 - rad ); i++ ) {
+							if( j >= 0 && j < TILE_HEIGHT ) {
+								new_x = i;
+								new_x %= TILE_WIDTH;
+								if( new_x < 0 ) new_x += TILE_WIDTH;
+								tile_updates.push( [ new_x, j, false ] );
+							}
+						}
+					}
+				}
+				if( g.active ) {
+					g.x = new_x;
+					g.y = new_y;
+					g.dx = new_dx;
+					g.dy = new_dy;
+				}
 			}
 		}
 
@@ -325,18 +447,35 @@ function win_load() {
 			}
 		}
 
+		function draw_grenade( id ) {
+			var g, a, offset = 0;
+			if( PLAYER_COUNT > id ) {
+				g = grenades[ id ];
+				if( g.active ) {
+					CTX_ENT.drawImage( SPRITES[ id * 4 + 2 ], g.x | 0, g.y | 0 );
+				}
+			}
+		}
+
 		function game_loop( ts ) {
 			curr_ts = ts;
 			if( last_ts == -1 ) last_ts = ts;
 			ticks = ts - last_ts;
 
+			var me = players[ ME ];
+
+			cursor_x = ( cursor_vx + me.x - VIEW_HWIDTH + PLAYER_HWIDTH ) % CANVAS_WIDTH;
+			if( cursor_x < 0 ) cursor_x += CANVAS_WIDTH;
+			cursor_y = ( cursor_vy + me.y - VIEW_HHEIGHT + PLAYER_HHEIGHT ) % CANVAS_HEIGHT;
+			if( cursor_y < 0 ) cursor_x += CANVAS_WIDTH;
+
 			var tile_update, tile_x, tile_y, tile_val;
 			if( RIGHT_MB_DOWN ) {
 				tile_x = ( cursor_x / TILE_SIZE ) | 0;
 				tile_y = ( cursor_y / TILE_SIZE ) | 0;
-				if( tile_y == TILE_HEIGHT - 1 || tile_y == 0 ) {
+				if( tile_y == TILE_HEIGHT - 1 ) {
 					tile_val = true;
-				} else {
+				} else if( tile_y >= 35 ) {
 					tile_val = tiles[ tile_y ][ ( tile_x + 1 ) % TILE_WIDTH ];
 					tile_val = tile_val || tiles[ tile_y ][ tile_x == 0 ? TILE_WIDTH - 1 : tile_x - 1 ];
 					tile_val = tile_val || tiles[ tile_y - 1 ][ tile_x ];
@@ -363,7 +502,17 @@ function win_load() {
 			update_player( 2 );
 			update_player( 3 );
 
+			update_grenade( 0 );
+			update_grenade( 1 );
+			update_grenade( 2 );
+			update_grenade( 3 );
+
 			CTX_ENT.clearRect( 0, 0, CANVAS_WIDTH, CANVAS_HEIGHT );
+			draw_grenade( 0 );
+			draw_grenade( 1 );
+			draw_grenade( 2 );
+			draw_grenade( 3 );
+
 			draw_player( 0 );
 			draw_player( 1 );
 			draw_player( 2 );
@@ -441,10 +590,6 @@ function win_load() {
 			} else {
 				p.right = true;
 			}
-			cursor_x = ( cursor_vx + p.x - VIEW_HWIDTH + PLAYER_HWIDTH ) % CANVAS_WIDTH;
-			if( cursor_x < 0 ) cursor_x += CANVAS_WIDTH;
-			cursor_y = ( cursor_vy + p.y - VIEW_HHEIGHT + PLAYER_HHEIGHT ) % CANVAS_HEIGHT;
-			if( cursor_y < 0 ) cursor_x += CANVAS_WIDTH;
 		}
 
 		function mousedown( e ) {
